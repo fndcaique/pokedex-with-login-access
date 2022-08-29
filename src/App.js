0018a8deb4ff8006cc19b02d9a2c7733744d5283
Fernando Soares <fndcaique@gmail.com>
2022-08-29 18:34:34 -0300

    add login route & user logged in verification

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
 
 import About from './components/About';
+import Login from './components/Login';
 import NotFound from './components/NotFound';
 import Pokedex from './components/Pokedex';
 import PokemonDetails from './components/PokemonDetails';
@@ -14,53 +15,75 @@ class App extends Component {
     super();
 
     this.state = {
+      username: '',
       pokemonList: pokemonListData,
     };
   }
 
+  onLogin = (username) => {
+    this.setState({ username });
+  };
+
   render() {
-    const { pokemonList } = this.state;
+    const { pokemonList, username } = this.state;
 
     return (
       <div className="App">
-        <nav>
-          <Link
-            className="link"
-            to="/"
-          >
-            Home
-          </Link>
-          <Link
-            className="link"
-            to="/about"
-          >
-            About
-          </Link>
-        </nav>
         {/* Coloque as rotas aqui.
         Lembre-se de utilizar o componente que faz apenas uma rota ser renderizada */}
-
         <Switch>
           <Route
-            exact
-            path="/"
-          >
-            <Pokedex pokemonList={pokemonList} />
-          </Route>
-          <Route
-            path="/about"
-            component={About}
-          />
-          <Route
-            path="/pokemon/:id"
+            path="/login"
             render={(routeProps) => (
-              <PokemonDetails
+              <Login
                 {...routeProps}
-                pokemons={pokemonList}
+                onLogin={this.onLogin}
               />
             )}
           />
-          <Route component={NotFound} />
+          <Route path="*">
+            <nav>
+              <Link
+                className="link"
+                to="/"
+              >
+                Home
+              </Link>
+              <Link
+                className="link"
+                to="/about"
+              >
+                About
+              </Link>
+            </nav>
+            <Switch>
+              <Route
+                path="/"
+                exact
+              >
+                <Pokedex pokemonList={pokemonList} />
+              </Route>
+              <Route
+                path="/about"
+                component={About}
+              />
+              <Route
+                path="/pokemon/:id"
+                render={(routeProps) => {
+                  if (username.length === 0) {
+                    return <Redirect to="/login" />;
+                  }
+                  return (
+                    <PokemonDetails
+                      {...routeProps}
+                      pokemons={pokemonList}
+                    />
+                  );
+                }}
+              />
+              <Route component={NotFound} />
+            </Switch>
+          </Route>
         </Switch>
       </div>
     );
